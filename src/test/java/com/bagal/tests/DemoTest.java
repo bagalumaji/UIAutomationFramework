c3560bf9bfaf2fe6722439e69f1846a1af2bf2c4
bagalumaji <bagalumaji@gmail.com>
2022-07-09 20:41:41 +0530

    updated demo test

--- src/test/java/com/bagal/tests/DemoTest.java
+++ src/test/java/com/bagal/tests/DemoTest.java
@@ -1,22 +1,11 @@
 package com.bagal.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import com.bagal.base.BaseTest;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
-public class DemoTest {
+public class DemoTest extends BaseTest {
     @Test
     public void demoTest(){
-        WebDriverManager.chromedriver().create();
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://google.com");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-
-        driver.quit();
 
     }
 }
