2694ef1e0785c44c0a1afc44d9bf4c0ebfecf40f
bagalumaji <bagalumaji@gmail.com>
2022-07-10 16:31:19 +0530

    updated base test

--- src/main/java/com/bagal/base/BaseTest.java
+++ src/main/java/com/bagal/base/BaseTest.java
@@ -6,9 +6,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
+
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
-        Driver.initDriver(BrowserType.SAFARI);
+        Driver.initDriver(BrowserType.CHROME);
     }
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
